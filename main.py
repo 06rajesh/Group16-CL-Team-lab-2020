92597c53222877b3bade20082dac37f25513b85a
06rajesh <abhi.crushefix@gmail.com>
2020-04-26 23:16:41 +0200

    Added sample output

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ if __name__ == '__main__':
     print("Macro")
     print(ev.macro)
 
+    # Output
+    ############################
+    # Micro
+    # {'precision': 0.9436296630565584, 'recall': 0.9435232186501222, 'fscore': 0.9435764378513547}
+    # Macro
+    # {'precision': 0.8512286029127887, 'recall': 0.8280785657646288, 'fscore': 0.8394940172773762}
